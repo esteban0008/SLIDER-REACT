db3ace1b19aa1087610e6f65e458f58834238445
esteban0008 <139926274+esteban0008@users.noreply.github.com>
2023-09-15 09:06:09 -0500

    cambios

---
+++ OneDrive/Escritorio/slider-react-main/src/Components/Common/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+
+const Navbar = ()=>{
+
+    return (
+        <ul style={{margin:"30", padding:"0", display:"flex",alignItems:"center", justifyContent:"center", gap:"70px", background:"white", width:"100%", height:"90px", fontSize:"40px", fontFamily:"fantasy",  }}>
+            <li style={{color:"green",  cursor:"pointer"}}>Copas</li>
+            <li style={{color:"green",  cursor:"pointer"}}>Nosotros</li>
+            <li style={{color:"green",  cursor:"pointer"}}>Partidos</li>
+        </ul>
+    )
+
+}
+
+export default Navbar
